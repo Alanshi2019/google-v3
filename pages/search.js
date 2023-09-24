96ac415c4cf6a4d912bd27b084213a96c17e0f12
Alanshi2019 <alanwork94@gmail.com>
2023-09-24 12:31:36 -0400

    create pagination component

--- pages/search.js
+++ pages/search.js
@@ -22,6 +22,7 @@ export default function Search({ results }) {
 export async function getServerSideProps(context) {
   // Loading mock data to avoid reaching the maximum requests limit by Google.
   const mockData = true;
+  const startIndex = context.query.start || "1";
   const data = mockData
     ? Response
     : await fetch(
@@ -29,7 +30,7 @@ export async function getServerSideProps(context) {
           process.env.API_KEY
         }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
           context.query.searchType && "&searchType=image"
-        }`
+        }&start=${startIndex}`
       ).then((response) => response.json());
   return {
     props: {
